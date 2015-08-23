28dcb44f439fe24b2fef50ff8049d13d8fa9344b
Michael Wong <michael.brian.wong@gmail.com>
2015-08-23 17:31:23 -0600

    Guessing working correctly

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,29 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-@@num = rand(100)
+SECRET_NUMBER = rand(100)
 
 get '/' do
-  "The secret number is #{@@num}."
+  guess = params["guess"]
+  message = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+end
+
+def check_guess(guess)
+  guess = guess.to_i
+  number = SECRET_NUMBER
+  range = 5
+  if guess == 0
+    message = ""
+  elsif guess > (number + range)
+    message = "Way Too High!"
+  elsif guess > number
+    message = "Too High"
+  elsif guess < (number - range)
+    message = "Way Too Low!"
+  elsif guess < number
+    message = "Too Low"
+  else
+    message = "The secrect number is #{number}. You got it right! "
+  end
 end

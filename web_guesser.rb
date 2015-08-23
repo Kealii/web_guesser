d8493624f6ff32a4646ccf150eebbd7399d9ab79
Michael Wong <michael.brian.wong@gmail.com>
2015-08-23 17:41:22 -0600

    added cheat mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,8 +5,12 @@ SECRET_NUMBER = rand(100)
 
 get '/' do
   guess = params["guess"]
+  cheat = params[:cheat]
   message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  cheat_message = cheat_mode(cheat)
+  erb :index, :locals => {:number        => SECRET_NUMBER,
+                          :message       => message,
+                          :cheat_message => cheat_message}
 end
 
 def check_guess(guess)
@@ -24,6 +28,12 @@ def check_guess(guess)
   elsif guess < number
     message = "Too Low"
   else
-    message = "The secrect number is #{number}. You got it right! "
+    message = "The secret number is #{number}. You got it right! "
+  end
+end
+
+def cheat_mode(cheat)
+  if cheat == 'true'
+    "The secret number is #{SECRET_NUMBER}."
   end
 end
